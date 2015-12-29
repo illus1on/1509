543a7544794791c3f66982620f7ae02553fa9556
guxiaotong <gxtkg@foxmail.com>
2015-12-29 15:30:52 +0800

    test git

---
+++ 1112d/1112d/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  1112d
+//
+//  Created by 顾小童 on 15/12/29.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ 1112d/1112d/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1112d
+//
+//  Created by 顾小童 on 15/12/29.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
